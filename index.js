1b8f0101fa46682346546246a395ec0807406590
matheuskipper <142031402+matheuskipper@users.noreply.github.com>
2023-11-09 09:10:06 -0300

    primeiro  commit de teste

---
+++ index.js
@@ -0,0 +1,6 @@
+let tabuada = Number(prompt('Insira a tabuada desejada!'));
+
+for (let contador = 0; contador <= 10; contador++) {
+    console.log(tabuada + ' x ' + contador + ' = ' + tabuada*contador);
+    
+};
