5e44504f40d64fe5d9d723ed9657fc92d4476483
matheuskipper <142031402+matheuskipper@users.noreply.github.com>
2023-11-10 11:44:48 -0300

    atualização do sistema

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ import { superLog } from "./superLog.js";
 console.log("teste");
 
 superLog('Estou testando o super log!', 'yellow');
+
+superLog('Estou atualizando o sistema com uma nova feature');
