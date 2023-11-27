2ce10a675893c892618b8eb912431fac50ef0b2d
matheuskipper <142031402+matheuskipper@users.noreply.github.com>
2023-11-27 10:50:15 -0300

    teste de um commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { superLog } from "./superLog.js";
 
 console.log("teste do console");
 
+console.log("Este é mais um teste");
+
 superLog('Estou testando o super log!', 'yellow');
 
 superLog('Estou atualizando o sistema com uma nova feature', 'white');
